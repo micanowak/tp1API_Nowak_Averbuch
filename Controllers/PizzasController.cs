563b9cb75d130878a0077c7f3dec9e23c6a23862
MIJAL MURIEL AVERBUCH <46754864@ort>
2023-03-17 12:03:54 -0300

    terminado

---
+++ Controllers/PizzasController.cs
@@ -0,0 +1,114 @@
+using Pizzas.API.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tp1API_Nowak_Averbuch.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PizzasController : Controller
+    {
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IActionResult respuesta;
+            List<Pizza> entityList;
+
+            entityList = BD.GetAll();
+            respuesta = Ok(entityList);
+            return respuesta;
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            IActionResult respuesta = null;
+            Pizza entity;
+
+            entity = BD.GetById(id);
+            if (entity == null)
+            {
+                respuesta = NotFound();
+            }
+            else
+            {
+                respuesta = Ok(entity);
+            }
+            return respuesta;
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            int intRowsAffected;
+            intRowsAffected = BD.Insert(pizza);
+            return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            IActionResult respuesta = null;
+            Pizza entity;
+            int intRowsAffected;
+            if (id != pizza.Id)
+            {
+                respuesta = BadRequest();
+            }
+            else
+            {
+                entity = BD.GetById(id);
+                if (entity == null)
+                {
+                    respuesta = NotFound();
+                }
+                else
+                {
+                    intRowsAffected = BD.UpdateById(pizza);
+                    if (intRowsAffected > 0)
+                    {
+                        respuesta = Ok(pizza);
+                    }
+                    else
+                    {
+                        respuesta = NotFound();
+                    }
+                }
+            }
+            return respuesta;
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteById(int id)
+        {
+            IActionResult respuesta = null;
+            Pizza entity;
+            int intRowsAffected;
+
+            entity = BD.GetById(id);
+            if (entity == null)
+            {
+                respuesta = NotFound();
+            }
+            else
+            {
+                intRowsAffected = BD.DeleteById(id);
+                if (intRowsAffected > 0)
+                {
+                    respuesta = Ok(entity);
+                }
+                else
+                {
+                    respuesta = NotFound();
+                }
+            }
+            return respuesta;
+        }
+    }
+}
